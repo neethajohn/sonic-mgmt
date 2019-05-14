e4e2faeca48addd924775b9a053f3299ecaa21cb
Neetha John <nejo@microsoft.com>
2019-05-14 22:48:10 +0000

    Merged PR 1675847: Include new SKU 7260cx3-Q64 in the minigraph facts
    Include new SKU 7260cx3-Q64 in the minigraph facts
    
    failures were seen prior to the fix while trying to run a test
    
    TASK [test : Gathering minigraph facts about the device] ***********************
    task path: /var/Networking-acs-sonic-mgmt/ansible/roles/test/tasks/sonic.yml:3
    fatal: [str-7260cx3-acs-7]: FAILED! => {"changed": false, "failed": true, "invocation": {"module_args": {"filename": null, "host": "str-7260cx3-acs-7"}, "module_name": "minigraph_facts"}, "msg": "Ethernet29/1"}

--- ansible/library/minigraph_facts.py
+++ ansible/library/minigraph_facts.py
@@ -486,7 +486,7 @@ def parse_xml(filename, hostname):
             port_alias_to_name_map["Ethernet%d/1" % i] = "Ethernet%d" % ((i - 5) * 4)
         for i in range(29, 37):
             port_alias_to_name_map["Ethernet%d" % i] = "Ethernet%d" % ((i - 5) * 4)
-    elif hwsku == "Arista-7260CX3-C64" or hwsku == "Arista-7170-64C":
+    elif hwsku in ["Arista-7260CX3-C64", "Arista-7170-64C", "Arista-7260CX3-Q64"]:
         for i in range(1, 65):
             port_alias_to_name_map["Ethernet%d/1" % i] = "Ethernet%d" % ((i - 1) * 4)
     elif hwsku == "Arista-7060CX-32S-C32" or hwsku == "Arista-7060CX-32S-Q32" or hwsku == "Arista-7060CX-32S-C32-T1" or hwsku == "Arista-7170-32CD-C32":
