26334b56ae875753806a655e59dab2a679dd82d3
Danny Allen <daall@microsoft.com>
2020-10-26 18:02:27 +0000

    Merged PR 3699962: [pretest] Add pretest step to use the internal nameserver
    [pretest] Add pretest step to use the internal nameserver
    
    Signed-off-by: Danny Allen <daall@microsoft.com>

--- tests/test_pretest.py
+++ tests/test_pretest.py
@@ -99,8 +99,8 @@ def collect_dut_lossless_prio(dut):
     if 'pfc_enable' not in port_qos_map[intf]:
         return []
 
-    result = [int(x) for x in port_qos_map[intf]['pfc_enable'].split(',')]    
-    return result 
+    result = [int(x) for x in port_qos_map[intf]['pfc_enable'].split(',')]
+    return result
 
 def collect_dut_all_prio(dut):
     config_facts = dut.config_facts(host=dut.hostname, source="running")['ansible_facts']
@@ -136,7 +136,7 @@ def test_collect_testbed_prio(duthosts, tbinfo):
         lossless_prio[dut.hostname] = collect_dut_lossless_prio(dut)
         lossy_prio[dut.hostname] = collect_dut_lossy_prio(dut)
 
-    prio_info = [all_prio, lossless_prio, lossy_prio]  
+    prio_info = [all_prio, lossless_prio, lossy_prio]
     file_names = [tbname + '-' + x + '.json' for x in ['all', 'lossless', 'lossy']]
     folder = 'priority'
 
@@ -149,3 +149,12 @@ def test_collect_testbed_prio(duthosts, tbinfo):
                 json.dump({ tbname : prio_info[i]}, yf, indent=4)
         except IOError as e:
             logger.warning('Unable to create file {}: {}'.format(filepath, e))
+
+def test_connect_to_internal_nameserver(duthost):
+    cmds = [
+        "echo \"nameserver 10.64.5.5\" > /etc/resolv.conf",
+        "sudo systemctl restart systemd-resolved"
+    ]
+
+    duthost.shell_cmds(cmds=cmds)
+
