a8f09db0e76f3845591545fd468bbd78ba2d981d
Sai Divya Anne <v-saidia@microsoft.com>
2020-12-12 00:00:16 +0000

    Merged PR 3891318: Onboard str2-7215-acs-1 Nokia DUT
    Related work items: #4953197

--- ansible/library/minigraph_facts.py
+++ ansible/library/minigraph_facts.py
@@ -598,6 +598,9 @@ def parse_xml(filename, hostname):
     elif hwsku == "et6448m":
         for i in range(0, 52):
             port_alias_to_name_map["Ethernet%d" % i] = "Ethernet%d" % i
+    elif hwsku == "Nokia-7215":
+        for i in range(0, 52):
+            port_alias_to_name_map["Ethernet%d" % i] = "Ethernet%d" % i
     elif hwsku == "newport":
         for i in range(0, 256, 8):
             port_alias_to_name_map["Ethernet%d" % i] = "Ethernet%d" % i
